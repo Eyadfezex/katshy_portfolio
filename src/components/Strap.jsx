1558a533e44bbe60437c5e90d06d3386251c0a77
Eyad Ahmed <Eyad932004@outlook.com>
2024-07-25 16:13:07 +0300

    [0015]-add some lines to Strap component

--- src/components/Strap.jsx
+++ src/components/Strap.jsx
@@ -35,6 +35,23 @@ const Strp = ({ className }) => {
           <span className="w-[1rem] h-[1rem] bg-white rounded-full"></span>
           <p className="uppercase">branding</p>
         </div>
+        <div
+          aria-hidden="true"
+          className="flex  items-center justify-center shrink-0	   font-roboto font-[400] gap-10 text-[32px] "
+        >
+          <span className="w-[1rem] h-[1rem] bg-white rounded-full"></span>
+          <p className="uppercase">creative design</p>
+          <span className="w-[1rem] h-[1rem] bg-white rounded-full"></span>
+          <p className="uppercase">ui/ux</p>
+          <span className="w-[1rem] h-[1rem] bg-white rounded-full"></span>
+          <p className="uppercase">marketing</p>
+          <span className="w-[1rem] h-[1rem] bg-white rounded-full"></span>
+          <p className="uppercase">motion</p>
+          <span className="w-[1rem] h-[1rem] bg-white rounded-full"></span>
+          <p className="uppercase">animation</p>
+          <span className="w-[1rem] h-[1rem] bg-white rounded-full"></span>
+          <p className="uppercase">branding</p>
+        </div>
       </div>
     </div>
   );
