d9547649b5c61c78701d208e9123364d47a4cede
Eyad Ahmed <Eyad932004@outlook.com>
2024-07-25 18:55:09 +0300

    [0016]-finish menu toggle animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,30 @@
+import DropMenu from "./Menu";
+import gsap from "gsap";
+import { useEffect, useState, useRef } from "react";
+
 const Nav = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const menuTimeline = useRef(null);
+  const handleMenuClick = () => {
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    if (!menuTimeline.current) {
+      menuTimeline.current = gsap
+        .timeline({ paused: true })
+        .to(".menu", { x: -400, duration: 0.5 });
+    }
+    toggle ? menuTimeline.current.play() : menuTimeline.current.reverse();
+  }, [toggle]);
+
   return (
-    <nav className="flex items-center px-[7rem] w-full py-7 justify-end ">
-      <div className="flex items-center text-white gap-4">
+    <nav className="flex items-center px-[7rem] w-full py-7 justify-end fixed z-10">
+      <div
+        className="flex items-center text-white gap-4 group cursor-pointer"
+        onClick={handleMenuClick}
+      >
         <svg
           width="2"
           height="51"
@@ -19,18 +42,19 @@ const Nav = () => {
               gradientUnits="userSpaceOnUse"
               gradientTransform="translate(1.375 25.5) rotate(90) scale(25.5 0.5)"
             >
-              <stop offset="0.375" stop-color="white" />
-              <stop offset="0.567708" stop-color="white" stop-opacity="0" />
+              <stop offset="0.375" stopColor="white" />
+              <stop offset="0.567708" stopColor="white" stopOpacity="0" />
             </radialGradient>
           </defs>
         </svg>
 
         <p className="uppercase text-[14px] font-poppins">Menu</p>
         <div>
-          <span className="bg-white w-[20px] h-[3px] block"></span>
-          <span className="bg-white w-[15px] h-[3px] block mt-1"></span>
+          <span className="bg-white w-[20px] h-[3px] block group-hover:w-[15px] duration-[0.3s]"></span>
+          <span className="bg-white w-[15px] h-[3px] block mt-1 group-hover:w-[20px] duration-[0.3s]"></span>
         </div>
       </div>
+      <DropMenu className="absolute top-[80%] right-[-20%] menu" />
     </nav>
   );
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,14 @@
-const DropMenu = () => {
+import gsap from "gsap";
+
+const DropMenu = ({ className, toggle }) => {
   const pathname = window.location.pathname;
   const currentPath = pathname.slice(1); // remove the first "/"
+
+  //
   return (
-    <div className="bg-strap px-10 py-16 w-[20rem] rounded-xl shadow-2xl">
+    <div
+      className={`bg-strap px-10 py-16 w-[20rem] rounded-xl shadow-2xl ${className}`}
+    >
       <div className="flex flex-col gap-[5rem] font-popping text-[24px] text-white">
         <a
           href="/"
