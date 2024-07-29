dd6b8a333b53ba85825a6cac0e0915981d2f7b3d
Eyad Ahmed <Eyad932004@outlook.com>
2024-07-29 05:49:30 +0300

    [0026]-format all files

--- src/components/loadingPage.jsx
+++ src/components/loadingPage.jsx
@@ -8,7 +8,7 @@ const LoadingPage = ({ className }) => {
     tl.fromTo(
       ".logo",
       { y: -500, opacity: 0, ease: "back.out" },
-      { y: 0, opacity: 1, duration: 1 }
+      { y: 0, opacity: 1, duration: 1 },
     );
     tl.to(".logo", { y: 500, duration: 1, ease: "back.in" }).delay(2);
 
