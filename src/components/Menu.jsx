bd2789d149bdc71cd6309813ccc92715430fb847
Eyad Ahmed <Eyad932004@outlook.com>
2024-07-29 04:29:10 +0300

    [0025]-edit menu content

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -15,22 +15,22 @@ const DropMenu = ({ className, toggle }) => {
           <p className="ml-8">Home</p>
         </a>
         <a
-          href="/about"
-          className={`uppercase before:absolute before:block before:top-8 relative  before:h-2 before:w-2 before:rounded-full hover:before:bg-second border-b ${currentPath === "about" ? "border-b-second before:bg-second" : "before:bg-white"}`}
-        >
-          <p className="ml-8">about us</p>
-        </a>
-        <a
           href="/services"
           className={`uppercase before:absolute before:block before:top-8 relative  before:h-2 before:w-2 before:rounded-full hover:before:bg-second border-b ${currentPath === "services" ? "border-b-second before:bg-second" : "before:bg-white"}`}
         >
-          <p className="ml-8">about us</p>
+          <p className="ml-8">services</p>
         </a>
         <a
           href="/portfolio"
           className={`uppercase before:absolute before:block before:top-8 relative  before:h-2 before:w-2 before:rounded-full hover:before:bg-second border-b ${currentPath === "portfolio" ? "border-b-second before:bg-second" : "before:bg-white"}`}
         >
           <p className="ml-8">portfolio</p>
+        </a>
+        <a
+          href="/about"
+          className={`uppercase before:absolute before:block before:top-8 relative  before:h-2 before:w-2 before:rounded-full hover:before:bg-second border-b ${currentPath === "about" ? "border-b-second before:bg-second" : "before:bg-white"}`}
+        >
+          <p className="ml-8">about us</p>
         </a>
         <a
           href="/contact"
