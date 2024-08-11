c2e4d6d9764f732dac44e3c1b703a1a19afe6e28
Eyad Ahmed <Eyad932004@outlook.com>
2024-08-11 21:25:58 +0300

    [0047]-make the site responsive

--- src/components/pageLabel.jsx
+++ src/components/pageLabel.jsx
@@ -3,7 +3,9 @@ const PageLabel = ({ label, className }) => {
     <div
       className={` relative after:absolute after:w-[3rem] after:h-[1px] after:bg-second after:top-[4.5rem]  font-poppins  ${className}`}
     >
-      <p className="text-white text-[40px] leading-[76px] uppercase">{label}</p>
+      <p className="text-white text-[30px] lg:text-[40px] leading-[76px] uppercase">
+        {label}
+      </p>
     </div>
   );
 };
